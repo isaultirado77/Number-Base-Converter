291bf49bcf0884e422e3d951df4680ea8cad3987
Isaul <tiradolisaul@gmail.com>
2024-09-20 12:55:30 -0700

    parseDecimalToTargetBase method on NumberBaseConverter added: testing

--- converter/model/NumberBaseConverter.java
+++ converter/model/NumberBaseConverter.java
@@ -28,7 +28,7 @@ public class NumberBaseConverter {
             } else if (Character.isLetter(digitChar)) {
                 coefficient = Character.toUpperCase(digitChar) - 'A' + 10;
             } else {
-                throw new IllegalArgumentException("Error! Invalid character in source number");
+                throw new IllegalArgumentException("Error! Invalid character in source number. ");
             }
 
             if (coefficient >= sourceBase) {
@@ -41,5 +41,24 @@ public class NumberBaseConverter {
             sum = sum.add(BigInteger.valueOf(coefficient).multiply(BigInteger.valueOf(sourceBase).pow(exponent)));
         }
         return sum;
+    }
+
+    private String parseDecimalToTargetBase(BigInteger decimal, int targetBase) {
+        StringBuilder sb = new StringBuilder();
+        BigInteger bigTargetBase = BigInteger.valueOf(targetBase);
+
+        while (!decimal.equals(BigInteger.ZERO)) {
+            BigInteger reminder = decimal.remainder(bigTargetBase);
+            decimal = decimal.divide(bigTargetBase);
+
+            // Convert the reminder to a character in the target base
+            if (reminder.compareTo(BigInteger.TEN) < 0) {
+                sb.append(reminder); // the current reminder is 0-9
+            } else {
+                // Convert 10-35 to 'A'-'Z'
+                sb.append((char) ('A' + reminder.intValue() - 10));
+            }
+        }
+        return sb.reverse().toString();
     }
 }
