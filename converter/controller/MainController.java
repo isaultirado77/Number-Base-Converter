4aed88e496a9a692851330021317be2c8c341b6a
Isaul <tiradolisaul@gmail.com>
2024-09-20 08:41:58 -0700

    init stage 3

--- converter/controller/MainController.java
+++ converter/controller/MainController.java
@@ -1,70 +1,7 @@
 package converter.controller;
 
-import converter.io.Printer;
-
 public class MainController {
 
-    public void run() {
-        while (true) {
-            String option = MainEngine.promptUserOption();
+    public void run(){};
 
-            if (option.equals("/exit")) break;
-
-            handleUserMenuOption(option);
-        }
-    }
-
-    private void handleUserMenuOption(String option) {
-        switch (option) {
-            case "/from":
-                fromOption();
-                break;
-            case "/to":
-                toOption();
-                break;
-            default:
-                Printer.println("Error! Invalid option.\n");
-                break;
-        }
-    }
-
-    private void fromOption() {
-        try {
-            int decimal = MainEngine.promptDecimalNumber();
-            int targetBase = MainEngine.promptTargetBase();
-            String targetBaseNumber = parseDecimalToTargetBase(decimal, targetBase);
-            Printer.printf("Conversion result: %s\n\n", targetBaseNumber);
-        } catch (IllegalArgumentException e) {
-            Printer.println(e.getMessage());
-        }
-    }
-
-    private String parseDecimalToTargetBase(int decimal, int targetBase) {
-        return switch (targetBase) {
-            case 2 -> MainEngine.decimalToBinary(decimal);
-            case 8 -> MainEngine.decimalToOctal(decimal);
-            case 16 -> MainEngine.decimalToHexadecimal(decimal);
-            default -> throw new IllegalArgumentException("Error! Incorrect target base. \n");
-        };
-    }
-
-    private void toOption() {
-        try {
-            String sourceNumber = MainEngine.promptSourceNumber();
-            int sourceBase = MainEngine.promptSourceBase();
-            String decimalNumber = parseToDecimal(sourceNumber, sourceBase);
-            Printer.printf("Conversion to decimal result: %s\n\n", decimalNumber);
-        } catch (NumberFormatException e) {
-            Printer.println(e.getMessage() + "\n");
-        }
-    }
-
-    private String parseToDecimal(String sourceNumber, int sourceBase) {
-        return switch (sourceBase) {
-            case 2 -> MainEngine.binaryToDecimal(sourceNumber);
-            case 8 -> MainEngine.octalToDecimal(sourceNumber);
-            case 16 -> MainEngine.hexaToDecimal(sourceNumber);
-            default -> throw new IllegalArgumentException("Error! Incorrect target base. \n");
-        };
-    }
 }
---
+++ converter/model/NumberWithBase.java
@@ -0,0 +1,5 @@
+package converter.model;
+
+public class NumberWithBase {
+
+}
